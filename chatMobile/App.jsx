d1314a018109254509adc1f075329248b26410db
Sakyto <procsssrus@mail.ru>
2023-09-17 01:29:21 +0400

    Add useEffect to prevent running code multiple times

--- chatMobile/App.jsx
+++ chatMobile/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import AuthMenu from './views/authMenu'
 import MainMenu from './views/mainMenu'
@@ -7,7 +7,10 @@ import io from 'socket.io-client'
 
 export default function App() {
   const [account, setAccount] = useState(null)
+
+  useEffect(() => {
     const socket = io('https://192.168.10.8:3000')
+  }, [])
 
   return (
     account == null ? <AuthMenu account={account} setAccount={setAccount}/> : <MainMenu/>
