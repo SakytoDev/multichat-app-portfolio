3dc2cf7c719b9c6f1b3058e84c3f25f96b511a86
Sakyto <procsssrus@mail.ru>
2023-09-16 21:32:33 +0400

    Add toast notifications

--- chatMobile/views/authMenu.jsx
+++ chatMobile/views/authMenu.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StatusBar, StyleSheet, View, Image } from 'react-native';
+import { StatusBar, StyleSheet, View, Image, ToastAndroid } from 'react-native';
 import { Text, TextInput, Button } from 'react-native-paper';
 import axios from 'axios';
 
