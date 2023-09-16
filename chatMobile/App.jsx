34e8e9122dc3c3ec7948a1627b6b114fb1492af7
Sakyto <procsssrus@mail.ru>
2023-09-17 01:28:36 +0400

    Add socket.io-client implementation

--- chatMobile/App.jsx
+++ chatMobile/App.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react'
 import AuthMenu from './views/authMenu'
 import MainMenu from './views/mainMenu'
 
+import io from 'socket.io-client'
+
 export default function App() {
   const [account, setAccount] = useState(null)
+    const socket = io('https://192.168.10.8:3000')
 
   return (
     account == null ? <AuthMenu account={account} setAccount={setAccount}/> : <MainMenu/>
