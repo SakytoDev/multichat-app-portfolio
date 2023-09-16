d4a93c917382e8a71f238eb153605ef864efe0c4
Sakyto <procsssrus@mail.ru>
2023-09-16 21:30:49 +0400

    Add main menu code

---
+++ chatMobile/views/mainMenu.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import { StatusBar, StyleSheet, View, Image } from 'react-native'
+import { Text, TextInput, Button } from 'react-native-paper'
+import axios from 'axios'
+
+const MainMenu = () => {
+
+}
+
+export default MainMenu;
