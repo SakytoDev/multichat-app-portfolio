609df3a88d0ebf6a579966c500b4f77f8566d2f6
Sakyto <procsssrus@mail.ru>
2023-09-26 02:23:15 +0400

    Remove debug code

--- chatMobile/scripts/sockets.js
+++ chatMobile/scripts/sockets.js
@@ -5,7 +5,7 @@ let singleton
 module.exports = {
     InitSocket: function() {
         singleton = io('https://192.168.10.8:3000')
-        console.log("Socket running")
+        //console.log("Socket running")
     },
     GetSingleton: () => singleton
 }
