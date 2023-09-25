acab82e204aca39737b27dc63beae6399e9016e4
Sakyto <procsssrus@mail.ru>
2023-09-26 02:19:22 +0400

    Feature: Stack Navigation implementation

--- chatMobile/App.jsx
+++ chatMobile/App.jsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import AuthMenu from './views/authMenu'
 import MainMenu from './views/mainMenu'
 
 import sockets from './scripts/sockets'
+
+const MainStack = createStackNavigator()
 
 export default function App() {
   const [account, setAccount] = useState(null)
@@ -13,6 +16,11 @@ export default function App() {
   }, [])
 
   return (
-    account == null ? <AuthMenu setAccount={setAccount}/> : <MainMenu account={account} setAccount={setAccount}/>
+      <NavigationContainer>
+        <MainStack.Navigator screenOptions={{ headerShown: false, presentation: 'transparentModal' }}>
+          <MainStack.Screen name="AuthMenu" component={AuthMenu}/>
+          <MainStack.Screen name="MainMenu" component={MainMenu}/>
+        </MainStack.Navigator>
+      </NavigationContainer>
   )
 }
